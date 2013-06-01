c7e2f0df9a1736e5418ac049e4270c09bd7b8108
Braden Shepherdson <braden.shepherdson@gmail.com>
2013-06-01 17:25:50 -0400

    Updating forth.fs for this system

--- forth.fs
+++ forth.fs
@@ -1,52 +1,13 @@
-\ 0 - Introduction
-\ DCPU-16 Forth
+\ Forth ARM
 \ (c) 2012 Braden Shepherdson
 \ Version 3
 
 \ This is a Forth system
-\ designed to run on DCPU-16
+\ designed to run on ARMv6
 \ systems. It consists of this
-\ floppy disk image and a
-\ binary to be loaded directly.
+\ source code file and a binary
+\ executable.
 
-
-
-
-
-
-\ 1 - Master Loader
-2 LOAD \ Core
-6 LOAD \ Control Structures
-12 LOAD \ ( comments
-13 LOAD \ Extra stack words
-14 LOAD \ Spaces and bases
-15 LOAD \ Output
-22 LOAD \ WITHIN and DEPTH
-23 LOAD \ Strings loader
-29 LOAD \ Defining Words Loader
-36 LOAD \ Welcome message
-37 LOAD \ Compiler Loader
-
-
-
-
-\ 2 - Core Loader
-3 LOAD \ Math and constants
-4 LOAD \ Literals and chars
-5 LOAD \ [COMPILE] and RECURSE
-
-
-
-
-
-
-
-
-
-
-
-
-\ 3 - Core - Math and constants
 : /MOD 2DUP MOD -ROT / ;
 
 : NL 10 ;
@@ -62,7 +23,6 @@
 : NOT 0= ;
 
 
-\ 4 - Core - Literals and chars
 \ LITERAL compiles LIT <foo>
 : LITERAL IMMEDIATE
     ' LIT , \ compile LIT
@@ -78,7 +38,6 @@
 : ')' [ CHAR ) ] LITERAL ;
 
 
-\ 5 - Core - [COMPILE] RECURSE
 \ Compiles IMMEDIATE words.
 : [COMPILE] IMMEDIATE
     WORD  \ get the next word
@@ -94,23 +53,7 @@
     ,         \ compile it
   ;
 
-\ 6 - Control Structures Loader
-\ Compiled code only!
-\ cond IF true-part THEN rest
-\ cond IF t ELSE f THEN
-\ CASE c1 OF ... ENDOF
-\   default ENDCASE
-
-7 LOAD \ IF
-8 LOAD \ THEN ELSE
-9 LOAD \ BEGIN UNTIL
-10 LOAD \ AGAIN UNLESS
-11 LOAD \ WHILE REPEAT
-33 LOAD \ CASE Statements
-40 LOAD \ DO LOOP
-
-
-\ 7 - IF
+\ Control structures - ONLY SAFE IN COMPILED CODE!
 \ cond IF t THEN rest
 \ -> cond 0BRANCH OFFSET t rest
 \ cond IF t ELSE f THEN rest
@@ -123,10 +66,6 @@
   ;
 
 
-
-
-
-\ 8 - THEN ELSE
 : THEN IMMEDIATE
     DUP
     HERE @ SWAP - \ calc offset
@@ -142,7 +81,8 @@
     HERE @ SWAP -
     SWAP !
   ;
-\ 9 - BEGIN UNTIL
+
+
 \ BEGIN loop condition UNTIL ->
 \ loop cond 0BRANCH OFFSET
 : BEGIN IMMEDIATE
@@ -156,9 +96,6 @@
 ;
 
 
-
-
-\ 10 - AGAIN and UNLESS
 \ BEGIN loop AGAIN, infinitely.
 : AGAIN IMMEDIATE
     ' BRANCH ,
@@ -174,7 +111,6 @@
 
 
 
-\ 11 - WHILE REPEAT
 \ BEGIN cond WHILE loop REPEAT
 : WHILE IMMEDIATE
     ' 0BRANCH ,
@@ -190,7 +126,8 @@
     HERE @ SWAP -
     SWAP !
   ;
-\ 12 - ( comments
+
+
 : ( IMMEDIATE
     1 \ tracking depth
     BEGIN
@@ -206,7 +143,7 @@
     DUP 0= UNTIL \ depth == 0
     DROP \ drop the depth
   ;
-\ 13 - Extra stack words
+
 : NIP ( x y -- y ) SWAP DROP ;
 : TUCK ( x y -- y x y )
     SWAP OVER ;
@@ -218,11 +155,6 @@
   ;
 
 
-
-
-
-
-\ 14 - Spaces and Bases
 \ writes n spaces to stdout
 : SPACES ( n -- )
     BEGIN
@@ -238,23 +170,7 @@
 : HEX ( -- ) 16 BASE ! ;
 
 
-\ 15 - Output LOADER
-16 LOAD \ U.
-17 LOAD \ .S stack printer
-18 LOAD \ UWIDTH
-19 LOAD \ U.R unsigned padded
-20 LOAD \ .R signed padded
-21 LOAD \ . U. ?
-
-
-
-
-
-
-
-
-
-\ 16 - U.
+\ Strings and numbers
 : U.  ( u -- )
     BASE @ /MOD \ ( width r q )
     ?DUP IF \ if q <> 0 then
@@ -270,7 +186,6 @@
     + EMIT
 ;
 
-\ 17 - .S prints the stack
 \ Debugging utility.
 : .S ( -- )
     DSP@ \ get stack pointer
@@ -285,8 +200,6 @@
 ;
 
 
-
-\ 18 - UWIDTH
 : UWIDTH ( u -- width )
     BASE @ / \ rem quot
     ?DUP IF   \ if quot <> 0
@@ -298,11 +211,6 @@
 
 
 
-
-
-
-
-\ 19 - U.R
 : U.R ( u width -- )
     SWAP   \ ( width u )
     DUP    \ ( width u u )
@@ -314,11 +222,6 @@
 ;
 
 
-
-
-
-
-\ 20 - .R
 \ Print padded, signed number
 : .R ( n width -- )
     SWAP DUP 0< IF
@@ -334,7 +237,7 @@
     SPACES SWAP \ u ?
     IF 45 EMIT THEN \ print -
     U. ;
-\ 21 - dot, master print, U.
+
 : . 0 .R SPACE ;
 \ Replace U.
 : U. U. SPACE ;
@@ -343,14 +246,6 @@
 
 
 
-
-
-
-
-
-
-
-\ 22 - WITHIN and DEPTH
 \ c a b WITHIN ->
 \   a <= c & c < b
 : WITHIN ( c a b -- ? )
@@ -366,39 +261,13 @@
     1- \ adjust for S0 on stack
 ;
 
-\ 23 - Strings Loader
-\ S" string" defines a string.
-\     ( -- addr len )
-\ C, appends a (machine) word
-\ to the current compiled word.
-24 LOAD \ C,
-25 LOAD \ String compiling
-26 LOAD \ String interpreting
-27 LOAD \ String master
-28 LOAD \ print string
 
-
-
-
-
-
-\ 24 - C,
 : C,
     HERE @ !
     1 HERE +! \ Increment HERE
 ;
 
 
-
-
-
-
-
-
-
-
-
-\ 25 - String compiling
 : .S_COMP
     ' LITSTRING ,
     HERE @ \ address
@@ -414,7 +283,6 @@
     1- SWAP ! \ set length
   ;
 
-\ 26 - String interpreted
 : .S_INTERP
     HERE @ \ temp space
     BEGIN
@@ -430,7 +298,6 @@
     SWAP     \ addr len
   ;
 
-\ 27 - String master
 : S" IMMEDIATE ( -- addr len )
     STATE @ IF \ compiling?
         .S_COMP
@@ -441,12 +308,6 @@
 
 
 
-
-
-
-
-
-\ 28 - print string
 : ." IMMEDIATE ( -- )
     STATE @ IF \ compiling?
         [COMPILE] S"
@@ -462,23 +323,8 @@
         AGAIN
     THEN
   ;
-\ 29 - Defining Words Loader
-30 LOAD \ Constants and Values
-31 LOAD \ ALLOT and Variables
-32 LOAD \ FORGET
-34 LOAD \ Execution tokens
-35 LOAD \ Arrays
 
 
-
-
-
-
-
-
-
-
-\ 30 - Constants and Values
 : CONSTANT
     WORD CREATE
     DOCOL , \ codeword
@@ -494,23 +340,25 @@
     ' EXIT ,
   ;
 
-\ 31 - ALLOT and Variables
-\ Allocates n words of memory
+\ Allocates n bytes of memory
 : ALLOT ( n -- addr )
     HERE @ SWAP \ here n
     HERE +!     \ add n to HERE
   ;
 
+\ Converts a number of cells into a number of bytes
+: CELLS ( n -- n ) 4 * ;
+
 \ Finally VARIABLE itself.
 : VARIABLE
-    1 ALLOT \ allocate 1 cell
+    1 CELLS ALLOT \ allocate 1 cell
     WORD CREATE
     DOCOL ,
     ' LIT ,
     , \ pointer from ALLOT
     ' EXIT ,
   ;
-\ 32 - FORGET
+
 \ FORGET is a horrible hack to
 \ deallocate memory.
 \ Sets HERE to the beginning of
@@ -524,9 +372,6 @@
   ;
 
 
-
-
-\ 33 - CASE Statements
 : CASE IMMEDIATE 0 ;
 : OF IMMEDIATE
     ' OVER ,
@@ -542,7 +387,6 @@
     [COMPILE] THEN REPEAT
 ;
 
-\ 34 - Execution Tokens
 : :NONAME
     0 0 CREATE \ nameless entry
     HERE @     \ current HERE
@@ -556,9 +400,7 @@
 : ['] IMMEDIATE ' LIT , ;
 
 
-
-
-\ 35 - Arrays
+\ Expects the user to specify the number of bytes, not cells.
 : ARRAY ( n -- )
   ALLOT >R
   WORD CREATE \ define the word
@@ -570,15 +412,9 @@
 ;
 
 
-
-
-
-
-\ 36 - Welcome message
 : WELCOME
-    ." DCPU-16 FORTH" CR
-    ." by Braden Shepherdson"
-    CR
+    ." FORTH ARM" CR
+    ." by Braden Shepherdson" CR
     ." version " VERSION . CR
 ;
 
@@ -586,61 +422,6 @@ WELCOME
 \ HIDE WELCOME
 
 
-
-
-
-
-\ 37 - Compiler loading
-\ Words for writing the
-\ compiled code to disk.
-38 LOAD \ Disk writers
-39 LOAD \ Main compiler
-
-
-
-
-
-
-
-
-
-
-
-\ 38 - Disk writers
-VARIABLE DISK_BLOCK
-
-: WRITEWORD ( n -- )
-    DUP 511 AND MMR + ( n da )
-    SWAP @ 
-    DUP 8 << SWAP 8 >> OR
-    OVER ! ( da )
-    511 AND 511 = IF
-        DISK_BLOCK @
-        WRITEBLOCK
-        1 DISK_BLOCK +!
-    THEN
-  ;
-
-
-\ 39 - Main compiler
-\ Takes the top address
-: COMPILER ( n -- )
-    0 DISK_BLOCK !
-    0 ( top 0 )
-    BEGIN
-        DUP WRITEWORD
-        1+
-        2DUP = ( here == 0 )
-    UNTIL
-    . DROP
-    DISK_BLOCK @ WRITEBLOCK
-  ;
-
-
-
-\ 40 - DO LOOP
-41 LOAD
-42 LOAD
 
 : DO IMMEDIATE \ lim start --
   HERE @
@@ -653,8 +434,6 @@ VARIABLE DISK_BLOCK
 ;
 
 
-
-\ 41 - +LOOP, LOOP
 : +LOOP IMMEDIATE \ inc --
   ' R> , ' R> , \ i s l
   ' SWAP , \ ils
@@ -670,7 +449,6 @@ VARIABLE DISK_BLOCK
 : LOOP IMMEDIATE \ --
   ' LIT , 1 , [COMPILE] +LOOP ;
 
-\ 42 - I, J
 : I \  -- i
   R> R> \ ret i
   DUP -ROT >R >R ;
